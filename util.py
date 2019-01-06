75b253a81563f688325ae7283ae9e89b320f55f4
Peter Olah <peter.olah@playtech.com>
2019-01-06 07:55:05 +0000

    2nd week

--- util.py
+++ util.py
@@ -1,8 +1,12 @@
 #!/usr/bin/python
 
 import base64
+import requests
+from os import path
 from pprint import pprint as pp
 
+
+downloaddir = 'downloads'
 
 encoded = {
 	'': '',
@@ -11,8 +15,11 @@ encoded = {
 decoded = dict()
 
 for word, encodedref in encoded.items():
-	decoded[word] = 'https://audio00.forvo.com/audios/mp3/{}'.format(
-		base64.b64decode(bytes(encodedref.replace("-","+").replace("_","/"), 'utf-8')).decode(encoding='utf-8')
-	)
+	_path = base64.b64decode(bytes(encodedref.replace("-","+").replace("_","/"), 'utf-8')).decode(encoding='utf-8')
+	_file = _path.split('/')[-1]
+	_url = 'https://audio00.forvo.com/audios/mp3/{}'.format(_path)
+	decoded[word] = _file
+	# response = requests.get(_url)
+	# open(path.join(downloaddir, _file) , 'wb').write(response.content)
 
 pp(decoded)
