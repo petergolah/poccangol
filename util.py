041d73e97f73d534c6cbe71c0379355deeec2623
Peter Olah <peter.olah@playtech.com>
2019-01-12 23:45:49 +0000

    week 3-4

--- util.py
+++ util.py
@@ -15,11 +15,13 @@ encoded = {
 decoded = dict()
 
 for word, encodedref in encoded.items():
+	if not word:
+		continue
 	_path = base64.b64decode(bytes(encodedref.replace("-","+").replace("_","/"), 'utf-8')).decode(encoding='utf-8')
 	_file = _path.split('/')[-1]
 	_url = 'https://audio00.forvo.com/audios/mp3/{}'.format(_path)
 	decoded[word] = _file
-	# response = requests.get(_url)
-	# open(path.join(downloaddir, _file) , 'wb').write(response.content)
+	response = requests.get(_url)
+	open(path.join(downloaddir, _file) , 'wb').write(response.content)
 
 pp(decoded)
